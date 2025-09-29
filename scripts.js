94b6473cd964dab3692622e2f613a9a1a57972c5
camom11e <49825585+camom11e@users.noreply.github.com>
2025-09-29 18:04:51 +0300

    fix структры html

--- scripts.js
+++ scripts.js
@@ -1,12 +1,1 @@
-let div = document.getElementById("div");
-let div2 = document.getElementById("div2");
-div.addEventListener("click",() => {
-	alert("Да, я")
-})
-div.addEventListener("click", (a)=>{});
-
-const foo = () => {
-	alert("Да, я 2")
-}
-div.removeEventListener("click", foo);
 
